8918c29ccd9d9b835d4669c785450565d54a6124
williamnf <wnfukunaga@gmail.com>
2016-09-26 12:49:54 +0100

    :heart:beautify:heart:

--- reader/MySceneGraph.js
+++ reader/MySceneGraph.js
@@ -1,43 +1,41 @@
-
 function MySceneGraph(filename, scene) {
-	this.loadedOk = null;
-	
-	// Establish bidirectional references between scene and graph
-	this.scene = scene;
-	scene.graph=this;
-		
-	// File reading 
-	this.reader = new CGFXMLreader();
+    this.loadedOk = null;
 
-	/*
-	 * Read the contents of the xml file, and refer to this class for loading and error handlers.
-	 * After the file is read, the reader calls onXMLReady on this object.
-	 * If any error occurs, the reader calls onXMLError on this object, with an error message
-	 */
-	 
-	this.reader.open('scenes/'+filename, this);  
+    // Establish bidirectional references between scene and graph
+    this.scene = scene;
+    scene.graph = this;
+
+    // File reading
+    this.reader = new CGFXMLreader();
+
+    /*
+     * Read the contents of the xml file, and refer to this class for loading and error handlers.
+     * After the file is read, the reader calls onXMLReady on this object.
+     * If any error occurs, the reader calls onXMLError on this object, with an error message
+     */
+
+    this.reader.open('scenes/' + filename, this);
 }
 
 /*
  * Callback to be executed after successful reading
  */
-MySceneGraph.prototype.onXMLReady=function() 
-{
-	console.log("XML Loading finished.");
-	var rootElement = this.reader.xmlDoc.documentElement;
-	
-	// Here should go the calls for different functions to parse the various blocks
-	var error = this.parseGlobalsExample(rootElement);
+MySceneGraph.prototype.onXMLReady = function() {
+    console.log("XML Loading finished.");
+    var rootElement = this.reader.xmlDoc.documentElement;
 
-	if (error != null) {
-		this.onXMLError(error);
-		return;
-	}	
+    // Here should go the calls for different functions to parse the various blocks
+    var error = this.parseGlobalsExample(rootElement);
 
-	this.loadedOk=true;
-	
-	// As the graph loaded ok, signal the scene so that any additional initialization depending on the graph can take place
-	this.scene.onGraphLoaded();
+    if (error != null) {
+        this.onXMLError(error);
+        return;
+    }
+
+    this.loadedOk = true;
+
+    // As the graph loaded ok, signal the scene so that any additional initialization depending on the graph can take place
+    this.scene.onGraphLoaded();
 };
 
 
@@ -45,53 +43,50 @@ MySceneGraph.prototype.onXMLReady=function()
 /*
  * Example of method that parses elements of one block and stores information in a specific data structure
  */
-MySceneGraph.prototype.parseGlobalsExample= function(rootElement) {
-	
-	var elems =  rootElement.getElementsByTagName('globals');
-	if (elems == null) {
-		return "globals element is missing.";
-	}
+MySceneGraph.prototype.parseGlobalsExample = function(rootElement) {
 
-	if (elems.length != 1) {
-		return "either zero or more than one 'globals' element found.";
-	}
+    var elems = rootElement.getElementsByTagName('globals');
+    if (elems == null) {
+        return "globals element is missing.";
+    }
 
-	// various examples of different types of access
-	var globals = elems[0];
-	this.background = this.reader.getRGBA(globals, 'background');
-	this.drawmode = this.reader.getItem(globals, 'drawmode', ["fill","line","point"]);
-	this.cullface = this.reader.getItem(globals, 'cullface', ["back","front","none", "frontandback"]);
-	this.cullorder = this.reader.getItem(globals, 'cullorder', ["ccw","cw"]);
+    if (elems.length != 1) {
+        return "either zero or more than one 'globals' element found.";
+    }
 
-	console.log("Globals read from file: {background=" + this.background + ", drawmode=" + this.drawmode + ", cullface=" + this.cullface + ", cullorder=" + this.cullorder + "}");
+    // various examples of different types of access
+    var globals = elems[0];
+    this.background = this.reader.getRGBA(globals, 'background');
+    this.drawmode = this.reader.getItem(globals, 'drawmode', ["fill", "line", "point"]);
+    this.cullface = this.reader.getItem(globals, 'cullface', ["back", "front", "none", "frontandback"]);
+    this.cullorder = this.reader.getItem(globals, 'cullorder', ["ccw", "cw"]);
 
-	var tempList=rootElement.getElementsByTagName('list');
+    console.log("Globals read from file: {background=" + this.background + ", drawmode=" + this.drawmode + ", cullface=" + this.cullface + ", cullorder=" + this.cullorder + "}");
 
-	if (tempList == null  || tempList.length==0) {
-		return "list element is missing.";
-	}
-	
-	this.list=[];
-	// iterate over every element
-	var nnodes=tempList[0].children.length;
-	for (var i=0; i< nnodes; i++)
-	{
-		var e=tempList[0].children[i];
+    var tempList = rootElement.getElementsByTagName('list');
 
-		// process each element and store its information
-		this.list[e.id]=e.attributes.getNamedItem("coords").value;
-		console.log("Read list item id "+ e.id+" with value "+this.list[e.id]);
-	};
+    if (tempList == null || tempList.length == 0) {
+        return "list element is missing.";
+    }
+
+    this.list = [];
+    // iterate over every element
+    var nnodes = tempList[0].children.length;
+    for (var i = 0; i < nnodes; i++) {
+        var e = tempList[0].children[i];
+
+        // process each element and store its information
+        this.list[e.id] = e.attributes.getNamedItem("coords").value;
+        console.log("Read list item id " + e.id + " with value " + this.list[e.id]);
+    }
 
 };
-	
+
 /*
  * Callback to be executed on any read error
  */
- 
-MySceneGraph.prototype.onXMLError=function (message) {
-	console.error("XML Loading Error: "+message);	
-	this.loadedOk=false;
+
+MySceneGraph.prototype.onXMLError = function(message) {
+    console.error("XML Loading Error: " + message);
+    this.loadedOk = false;
 };
-
-
