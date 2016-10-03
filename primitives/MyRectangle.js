16545cee9bf8ed0008cc9703db9fb5e7851b7c81
williamnf <wnfukunaga@gmail.com>
2016-10-03 13:22:16 +0100

    added rectangle primitive :smirk:

---
+++ primitives/MyRectangle.js
@@ -0,0 +1,48 @@
+/**
+ * MyRectangle
+ * @constructor
+ */
+
+ function MyRectangle(scene, xmlNode){
+   CGFobject.call(this,scene);
+
+   this.parseAttributes(xmlNode);
+   this.initBuffers();
+
+ }
+
+ MyRectangle.prototype = Object.create(CGFobject.prototype);
+ MyRectangle.prototype.constructor = MyRectangle;
+
+
+ MyRectangle.prototype.initBuffers = function() {
+   this.vertices = [
+  	this.x1, this.y1, 0,
+  	this.x2, this.y1, 0,
+  	this.x1, this.y2, 0,
+  	this.x2, this.y2, 0
+  	];
+
+  	this.indices = [
+  	0, 1, 2,
+  	3, 2, 1
+  	];
+
+   this.normals = [
+   0, 0, 1,
+   0, 0, 1,
+   0, 0, 1,
+   0, 0, 1
+   ];
+
+   this.primitiveType = this.scene.gl.TRIANGLES;
+   this.initGLBuffers();
+};
+
+
+MyRectangle.prototype.parseAttributes = function (xmlNode){
+  this.x1 = this.scene.reader.getFloat(xmlNode, 'x1');
+  this.y1 = this.scene.reader.getFloat(xmlNode, 'y1');
+  this.x2 = this.scene.reader.getFloat(xmlNode, 'x2');
+  this.y2 = this.scene.reader.getFloat(xmlNode, 'y2');
+};
