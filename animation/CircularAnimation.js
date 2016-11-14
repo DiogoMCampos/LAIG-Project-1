dd80c3fe9fbe2118fd8e5f9e5e587e9273a8a882
Diogo Campos <d.diogocampos@gmail.com>
2016-11-14 22:38:45 +0000

    Creating circular animation transformation matrix

--- animation/CircularAnimation.js
+++ animation/CircularAnimation.js
@@ -1,6 +1,46 @@
-function CircularAnimation(id, duration) {
+function CircularAnimation(id, duration, centerX, centerY, centerZ, radius, startAng, rotAng) {
     Animation.call(this, id, duration);
+
+    this.center = vec3.fromValues(this.centerX, this.centerY, this.centerZ);
+    this.radius = radius;
+
+    this.startAng = startAng * 180 / Math.PI;
+    this.rotAng = rotAng * 180 / Math.PI;
+
+    this.currAng = startAng;
+    this.finalAng = this.startAng + this.rotAng;
+
+    this.angVar = this.rotAng / this.duration;
+
+    this.currTime = -1;
+    this.finished = false;
 }
 
 CircularAnimation.prototype = Object.create(Animation.prototype);
 CircularAnimation.prototype.constructor = CircularAnimation;
+
+CircularAnimation.prototype.getTransformationMatrix = function(time) {
+    var timeVar;
+
+    if (this.currTime === -1) {
+        timeVar = 0;
+    } else {
+        timeVar = time - this.currTime;
+    }
+
+    this.currTime = timeVar;
+
+    var angle = this.startAng + (this.angVar * timeVar);
+
+    var matrix = mat4.create();
+    mat4.translate(matrix, matrix, this.center);
+    mat4.rotate(matrix, matrix, ver3.fromValues(0, -angle, 0));
+    mat4.translate(matrix, matrix, vec3.fromValues(this.radius, 0, 0));
+
+    this.currAng += angle;
+    if (this.currAng >= this.finalAng) {
+        this.finished = true;
+    }
+
+    return matrix;
+};
